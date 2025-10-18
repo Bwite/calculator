f3a0d3ab2754ccd026ad198f0e8c5bc91bf349fc
Bwite <bwite211@gmail.com>
2025-10-18 07:42:13 +0100

    made some progress

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // your JavaScript file
+
+let statement = [];
 function press(instruction){
 const container = document.querySelector("#glass");
 
@@ -7,6 +9,32 @@ content.classList.add("content");
 content.textContent = instruction;
 
 glass.appendChild(content);
+
+if (instruction !=='='){
+statement.push(instruction);
+}else{
+   const threshold = ['+' , '-' , '*' , '/'];
+    const leftside = statement.filter(num =>  threshold.includes(num));
+    //const rightside = statement.filter(num => num < threshold);
+    console.log("on the left : ", leftside); // Output: [ 8, 10, 7 ]
+    const stringWithoutCommas = statement.join(''); // Joins elements with no separator
+    console.log(stringWithoutCommas);
+    const index = stringWithoutCommas.indexOf(leftside);  // Gets the first index where a space occours
+    const a = stringWithoutCommas.slice(0, index); // Gets the first part
+    const b = stringWithoutCommas.slice(index + 1);
+
+    console.log(a);
+    console.log(b);
+    if (leftside =='+'){
+      console.log('the sum is'+add(a,b));
+    } else if (leftside == '-') {
+      console.log('this is subtraction' + subtract(a,b));
+    }
+
+}
+
+console.log(statement);
+
 }
 //not working rn
 function clear(){
@@ -19,22 +47,31 @@ content.Remove(content);
 
 
 const add = function(a,b) {
+    a=parseInt(a);
+    b=parseInt(b);
     console.log(a+b)
     return a+b;
 };
 
 const subtract = function(a,b) {
+   a=parseInt(a);
+   b=parseInt(b);
 	return a-b;
 };
 const divide = function(a,b){
+    a=parseInt(a);
+    b=parseInt(b);
     return a/b;
 }
 
-const multiply = function(numbers) {
-  const product = numbers.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
-  return product;
+const multiply = function(a,b) {
+   a=parseInt(a);
+   b=parseInt(b);
+ 
+  return a*b;
 };
 function getFromDom(){
+  
   const nodelist = document.querySelectorAll('.content');
   console.log(nodelist);
   
